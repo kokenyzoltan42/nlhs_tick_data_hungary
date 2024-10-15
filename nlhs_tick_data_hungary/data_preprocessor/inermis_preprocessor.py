e2ab6a05698043bbca845c1ea4811e8db4920cb7
zoltanocska <zolysta@gmail.com>
2024-10-15 11:45:20 +0200

    changed structure

--- nlhs_tick_data_hungary/data_preprocessor/inermis_preprocessor.py
+++ nlhs_tick_data_hungary/data_preprocessor/inermis_preprocessor.py
@@ -5,6 +5,8 @@ class InermisPreprocessor:
     def __init__(self, data):
         self.data = data
 
+        self.fixing_columns()
+
     def fixing_columns(self):
         self.data['Bacteria'] = self.data['18×173']
         self.data = self.data.drop(columns=['18×173'])
@@ -33,31 +35,31 @@ class InermisPreprocessor:
         df = df.apply(pd.to_numeric, errors='coerce', downcast='integer')
         return df
 
-    def get_winter_tick_data(self, data: pd.DataFrame, gender: str, year: str, month: str):
+    def get_winter_tick_data(self, gender: str, year: str, month: str):
         # Se év, se hónap -> teljes adattábla
         if year == '' and month == '':
-            tick_data = self.select_gender(df=data, gender=gender)
+            tick_data = self.select_gender(df=self.data, gender=gender)
             return tick_data.astype(float)
 
         # Multiindexeés a könnyebb kezelhetőségért
-        data.columns = pd.MultiIndex.from_tuples(
-            [(year, month, individual) for year, month, individual in data.columns],
+        self.data.columns = pd.MultiIndex.from_tuples(
+            [(year, month, individual) for year, month, individual in self.data.columns],
             names=['Year', 'Month', 'Gender']
         )
 
         # Csak hónap van megadva -> az összes év (amiben benne van ez a hónap) összevonjuk(?)
         if year == '' and month != '':
-            available_years = [y for y in ['2022', '2023'] if (y, month) in data.columns]
-            concatonated = pd.concat([data[(y, month)] for y in available_years], axis=1)
+            available_years = [y for y in ['2022', '2023'] if (y, month) in self.data.columns]
+            concatonated = pd.concat([self.data[(y, month)] for y in available_years], axis=1)
 
         # Csak év van megadva -> azon évbeli összes hónap
         elif year != '' and month == '':
-            concatonated = pd.concat([data[(year, m)] for m in ['January', 'October', 'November', 'December'] if
-                                      (year, m) in data.columns], axis=1)
+            concatonated = pd.concat([self.data[(year, m)] for m in ['January', 'October', 'November', 'December'] if
+                                      (year, m) in self.data.columns], axis=1)
 
         # Meg van adva az év és a hónap is
         else:
-            concatonated = data[(year, month)]
+            concatonated = self.data[(year, month)]
 
         # Filter by gender and return the result
         tick_data = self.select_gender(df=concatonated, gender=gender)
