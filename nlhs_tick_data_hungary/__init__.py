1bb2b106e1233d6d1b79b8d7ed6755eec9009489
zoltanocska <zolysta@gmail.com>
2024-10-28 00:28:56 +0100

    testing json reading

--- nlhs_tick_data_hungary/__init__.py
+++ nlhs_tick_data_hungary/__init__.py
@@ -1,4 +1,4 @@
 import os
 
 project_dir = os.path.dirname(os.path.realpath(__file__)).rstrip("nlhs_tick_data_hungary")
-url_path = os.path.join(project_dir, "configs")
+url_path = os.path.join(project_dir, "nlhs_tick_data_hungary", "configs")
