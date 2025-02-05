5d3997881d5260b96e1c8c8caa56c6e6546dda07
zoltanocska <zolysta@gmail.com>
2025-02-05 10:27:03 +0100

    type annotations

--- nlhs_tick_data_hungary/data/data_preprocessing/pilis/pilis_tick_data_preprocessor.py
+++ nlhs_tick_data_hungary/data/data_preprocessing/pilis/pilis_tick_data_preprocessor.py
@@ -1,17 +1,20 @@
+from typing import Tuple
+
 import pandas as pd
 
 from nlhs_tick_data_hungary.data.data_preprocessing.pilis.column_transformer import ColumnTransformer
 from nlhs_tick_data_hungary.data.data_preprocessing.pilis.index_transformer import IndexTransformer
 
+
 class PilisTickDataPreprocessor:
     def __init__(self, raw_data: pd.DataFrame):
         self.raw_data = raw_data
 
-    def run(self) -> None:
+    def run(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
         pass
 
     def fill_missing_data(self) -> pd.DataFrame:
         pass
 
-    def sum_species(self, df):
+    def sum_species(self):
         pass
