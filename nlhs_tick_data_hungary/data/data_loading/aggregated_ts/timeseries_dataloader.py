51d7beee1e487166b3047695009a4df46c073b70
zoltanocska <zolysta@gmail.com>
2025-02-06 23:12:49 +0100

    finalized structure

--- nlhs_tick_data_hungary/data/data_loading/aggregated_ts/timeseries_dataloader.py
+++ nlhs_tick_data_hungary/data/data_loading/aggregated_ts/timeseries_dataloader.py
@@ -1,6 +1,20 @@
+from nlhs_tick_data_hungary.data.data_loading.aggregated_ts.lyme_dataloader import LymeDataLoader
+from nlhs_tick_data_hungary.data.data_loading.aggregated_ts.pilis_tick_dataloader import PilisTickDataLoader
+from nlhs_tick_data_hungary.data.data_loading.aggregated_ts.rainfall_dataloader import RainfallDataLoader
+
+
 class TimeseriesDataLoader:
     def __init__(self):
-        pass
+        self.data = {}
 
-    def run(self) -> dict:
-        pass
+    # TODO: change implementation diagram
+    def run(self) -> None:
+        lyme_dataloader = LymeDataLoader()
+        pilis_dataloader = PilisTickDataLoader()
+        rainfall_dataloader = RainfallDataLoader()
+
+        self.data = {
+            'lyme_data': lyme_dataloader.result,
+            'pilis_data': pilis_dataloader.result,
+            'rainfall_data': rainfall_dataloader.result
+        }
