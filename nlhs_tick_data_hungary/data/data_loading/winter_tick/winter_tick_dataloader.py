9ad06d58088b15866c1ebde7f9a7981092558e0c
zoltanocska <zolysta@gmail.com>
2025-02-07 12:04:45 +0100

    Added docstrings and comments

--- nlhs_tick_data_hungary/data/data_loading/winter_tick/winter_tick_dataloader.py
+++ nlhs_tick_data_hungary/data/data_loading/winter_tick/winter_tick_dataloader.py
@@ -8,23 +8,70 @@ from ...data_preprocessing.winter_tick.winter_tick_data_preprocessor import Wint
 
 
 class WinterTickDataLoader:
+    """
+    A class responsible for loading and preprocessing winter tick data.
+
+    This class loads raw winter tick data from a Google Sheet, preprocesses it using the `WinterTickDataPreprocessor`,
+    and stores the resulting processed data.
+
+    result (dict): A dictionary to store the processed winter tick data.
+    """
+
     def __init__(self):
+        """
+        Initializes the WinterTickDataLoader class by setting up the `result` attribute to store processed data
+        and loading the links to the necessary datasets.
+
+        The links are loaded from the `links.json` file, which contains URLs to the raw data.
+        """
         self.result = {}
 
+        # Load the URLs from the config file (links.json)
         with open(config_path + f'/links.json', 'r+') as file:
             self.links = json.load(file)
 
     def run(self) -> None:
+        """
+        Runs the data loading and preprocessing pipeline.
+
+        Loads the raw winter tick data from the Google Sheet using the `load_raw_data` method then preprocesses the
+        raw data using the `preprocess_data` method and stores the result in the `result` attribute.
+        """
+        # Load raw data
         raw_data = self.load_raw_data()
+
+        # Preprocess the raw data and store the result
         self.result = self.preprocess_data(raw_data=raw_data)
 
     def load_raw_data(self) -> pd.DataFrame:
+        """
+        Loads raw winter tick data from a Google Sheet.
+        This method uses the `GoogleSheetDataLoader` class to fetch data from the provided URL stored in `links.json`.
+
+        :return: A pandas DataFrame containing the raw winter tick data.
+        """
+        # Initialize GoogleSheetDataLoader with the URL from the configuration file
         dataloader = GoogleSheetDataLoader(url=self.links['winter_tick'])
+
+        # Load the data from the Google Sheet and return it as a DataFrame
         return dataloader.load_data()
 
     @staticmethod
     def preprocess_data(raw_data: pd.DataFrame) -> dict:
+        """
+        Preprocesses the raw winter tick data.
+
+        This method creates an instance of `WinterTickDataPreprocessor`, which handles the data cleaning,
+        transformation, and other preprocessing steps. It then returns the processed data.
+
+        :param raw_data: A pandas DataFrame containing the raw winter tick data to be processed.
+        :return: A dictionary containing the processed winter tick datas.
+        """
+        # Initialize the preprocessor with the raw data
         preprocessor = WinterTickDataPreprocessor(data=raw_data)
+
+        # Run the preprocessing steps
         preprocessor.run()
 
+        # Return the processed result
         return preprocessor.result
