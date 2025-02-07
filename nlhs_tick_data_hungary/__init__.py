ce10ddeccfc49ac13e5fdbf3bd9ed686d1f2fd02
zoltanocska <zolysta@gmail.com>
2025-02-07 01:10:47 +0100

    added new path for long lists (group names)

--- nlhs_tick_data_hungary/__init__.py
+++ nlhs_tick_data_hungary/__init__.py
@@ -2,3 +2,4 @@ import os
 
 project_dir = os.path.dirname(os.path.realpath(__file__)).rstrip("nlhs_tick_data_hungary")
 url_path = os.path.join(project_dir, "nlhs_tick_data_hungary", "configs")
+long_list_path = os.path.join(project_dir, "nlhs_tick_data_hungary", "long_lists.json")
