9baeb390fcca21cd4dcf49cb4d1c03b536236a12
zoltanocska <zolysta@gmail.com>
2025-02-07 11:15:36 +0100

    Added docstrings, comments

--- nlhs_tick_data_hungary/data/data_loading/aggregated_ts/pilis_tick_dataloader.py
+++ nlhs_tick_data_hungary/data/data_loading/aggregated_ts/pilis_tick_dataloader.py
@@ -9,27 +9,71 @@ from nlhs_tick_data_hungary.data.data_preprocessing.pilis.pilis_tick_data_prepro
 
 
 class PilisTickDataLoader:
+    """
+    A class to load and preprocess tick data from a Google Sheets source.
+
+    result (dict): A dictionary that stores the processed tick data after preprocessing.
+    links (dict): A dictionary containing the URLs for the raw data (in Google Sheets).
+    """
+
     def __init__(self):
+        """
+        Initializes the PilisTickDataLoader by loading the URLs for the tick data file and then runs the data loading
+        and preprocessing.
+        """
         self.result = None
 
+        # Load the links for the Pilis tick data from the 'links.json' config file
         with open(config_path + f'/links.json', 'r+') as file:
             self.links = json.load(file)
 
         self.run()
 
     def run(self) -> None:
+        """
+        Runs the data loading and preprocessing pipeline:
+        - Loads the raw Pilis tick data from Google Sheets.
+        - Preprocesses the data and stores the result.
+        """
+        # Load the raw tick data from the Google Sheets using the links from the config file
         raw_pilis_tick_data = self.load_raw_data()
+
+        # Preprocess the raw data and store it
         self.result = self.preprocess_data(raw_pilis_tick_data=raw_pilis_tick_data)
 
     def load_raw_data(self) -> pd.DataFrame:
+        """
+        Loads the raw Pilis tick data from the specified Google Sheets file.
 
+        The raw data is loaded using the GoogleSheetDataLoader class, which downloads
+        the data from a URL and returns it as a pandas DataFrame.
+
+        Return:
+            pd.DataFrame: The raw Pilis tick data loaded from Google Sheets.
+        """
+        # Use the GoogleSheetDataLoader to download and load the data from the specified URL
         dataloader = GoogleSheetDataLoader(url=self.links['pilis_tick'])
+
         return dataloader.load_data()
 
     @staticmethod
     def preprocess_data(raw_pilis_tick_data: pd.DataFrame) -> dict[DataFrame, DataFrame]:
-        preprocessor = PilisTickDataPreprocessor(raw_pilis_tick_data=raw_pilis_tick_data)
+        """
+        Preprocesses the raw Pilis tick data by running it through the PilisTickDataPreprocessor.
 
+        This method initializes the PilisTickDataPreprocessor to processes the raw data,
+        and returns the processed result. The result is a dictionary with two dataframes:
+        - dataframe with modified (monthly) indices
+        - dataframe with the original dates
+
+        raw_pilis_tick_data (pd.DataFrame): The raw tick data to be preprocessed.
+
+        Return:
+            dict: A dictionary containing the preprocessed data. The keys will likely correspond to different stages or
+            categories of the processed data, and the values will be the corresponding pandas DataFrames.
+        """
+        # Initialize and run the preprocessor with the raw tick data
+        preprocessor = PilisTickDataPreprocessor(raw_pilis_tick_data=raw_pilis_tick_data)
         preprocessor.run()
 
         return preprocessor.result
