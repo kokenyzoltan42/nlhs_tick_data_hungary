08e5de6275f5fd14d857c867c47b47189561568e
zoltanocska <zolysta@gmail.com>
2025-02-17 23:48:01 +0100

    created structure of the cooccurence graph preprocessor

--- nlhs_tick_data_hungary/graph/graph_preparation/cooccurence_graph_preprocessor.py
+++ nlhs_tick_data_hungary/graph/graph_preparation/cooccurence_graph_preprocessor.py
@@ -1,3 +1,26 @@
 """
 Itt lesz a különbségek kezelése is, meg a crosstable létrehozása (assistingmethods példányosítása) pl.
 """
+import numpy as np
+
+from nlhs_tick_data_hungary.utils.assisting_methods import AssistingMethods
+
+
+class CooccurenceGraphPreprocessor:
+    def __init__(self, df, type_of_data, percentage, year, month):
+        self.df = df
+        self.type_of_data = type_of_data
+        self.percentage = percentage
+        self.year = year
+        self.month = month
+
+        self.num_of_samples = None
+        self.epsilon = 1e-5  # Small value to prevent division by zero
+
+        self.preprocessed_df = None
+
+    def create_crosstable_based_on_type_of_data(self):
+        pass
+
+    def convert_to_percentage(self):
+        pass
