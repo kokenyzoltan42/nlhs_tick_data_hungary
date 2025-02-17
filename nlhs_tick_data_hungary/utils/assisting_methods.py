4bbdfe2da121278ec783f8d07141edc61c92e432
zoltanocska <zolysta@gmail.com>
2025-02-17 23:47:33 +0100

    added new function and a todo

--- nlhs_tick_data_hungary/utils/assisting_methods.py
+++ nlhs_tick_data_hungary/utils/assisting_methods.py
@@ -2,9 +2,21 @@ import numpy as np
 
 
 class AssistingMethods:
+    # TODO: type annotations
     @staticmethod
     def create_crosstable(df):
         # out = pd.get_dummies(data=df.iloc[:, 1:].stack()).sum().ne(0).astype(int)
         final = df.dot(df.T).astype(float)
         np.fill_diagonal(final.values, np.nan)
         return final
+
+    @staticmethod
+    def select_type(df, to_type):
+        if to_type == 'Hím':
+            df = df.filter(like='Male')
+        elif to_type == 'Nőstény':
+            df = df.filter(like='Female')
+        elif to_type == '':
+            pass
+
+        return df
