214b1a61c73fa9ba205750b26acefa5501e16ae7
zoltanocska <zolysta@gmail.com>
2025-03-05 00:09:17 +0100

    Filled most of the functions in NetworkAnalyzer

--- nlhs_tick_data_hungary/network/network_analyzing/network_analyzer.py
+++ nlhs_tick_data_hungary/network/network_analyzing/network_analyzer.py
@@ -1,43 +1,61 @@
-# TODO: calc_ metódusokat ABC-sorrendbe tenni
+import networkx as nx
+
+
 class NetworkAnalyzer:
-    def __init__(self):
-        pass
+    def __init__(self, config: dict, network: nx.Graph):
+        self.config = config
+        self.network = network
 
-    def calc_network_diameter(self):
-        pass
-
-    def calc_modularity(self):
-        pass
-
-    def calc_average_degree_connectivity(self):
-        pass
-
-    def calc_number_of_communities(self):
-        pass
-
-    def calc_number_of_triangles(self):
-        pass
-
-    def calc_largest_connected_component(self):
-        pass
-
-    def calc_average_path_length(self):
-        pass
-
-    def calc_degree_centrality(self):
-        pass
-
-    def calc_betweenness_centrality(self):
-        pass
-
-    def calc_closeness_centrality(self):
-        pass
-
-    def calc_eigenvector_centrality(self):
-        pass
-
-    def calc_hub_taxa(self):
-        pass
+        self.result: (dict | None) = None
 
     def run(self):
+        for func_name, enabled in self.config.items():
+            if enabled and hasattr(self, func_name):
+                method = getattr(self, func_name)
+                if callable(method):
+                    result_key = func_name.replace("calc_", "")
+                    self.result[result_key] = method()
+
+    def calc_network_diameter(self) -> int:
+        return nx.diameter(self.network, weight='weight')
+
+    def calc_modularity(self):
+        return nx.algorithms.community.quality.modularity(
+            G=self.network,
+            communities=nx.algorithms.community.label_propagation_communities(self.network)
+        )
+
+# TODO: `calc_average_degree_connectivity` eltávolítása az implementációs diagram leírásából
+#  és talán a hub_taxa-t is
+
+    def calc_number_of_communities(self) -> int:
+        return len(nx.algorithms.community.label_propagation_communities(self.network))
+
+    def calc_number_of_triangles(self) -> int:
+        return nx.algorithms.cluster.triangles(G=self.network)
+
+    def calc_largest_connected_component(self) -> set:
+        # Bármennyire is írja, hogy Sized, ez `set`
+        # TODO: leellenőrizni
+        return max(nx.connected_components(G=self.network), key=len)
+
+    def calc_average_path_length(self) -> float:
+        # default method (ha van weight): ‘dijkstra’.
+        return nx.algorithms.shortest_paths.generic.average_shortest_path_length(G=self.network, weight='weight')
+
+    def calc_degree_centrality(self) -> dict:
+        return nx.algorithms.centrality.degree_centrality(G=self.network)
+
+    def calc_betweenness_centrality(self) -> dict:
+        return nx.algorithms.centrality.betweenness_centrality(G=self.network,
+                                                               weight='weight')
+
+    def calc_closeness_centrality(self) -> dict:
+        return nx.algorithms.centrality.closeness_centrality(G=self.network)
+
+    def calc_eigenvector_centrality(self) -> dict:
+        return nx.algorithms.centrality.eigenvector_centrality(G=self.network,
+                                                               weight='weight')
+
+    def calc_hub_taxa(self):
         pass
