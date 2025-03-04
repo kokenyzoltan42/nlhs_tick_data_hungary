045f1543d8600a27752aad013832d5d49b1dadab
zoltanocska <zolysta@gmail.com>
2025-03-05 00:58:41 +0100

    Finished most of the creation of the \Omega matrix (missing comments, docstrings)

--- nlhs_tick_data_hungary/network/network_preparation/sparcc/basis_variance_calculator.py
+++ nlhs_tick_data_hungary/network/network_preparation/sparcc/basis_variance_calculator.py
@@ -1,6 +1,30 @@
+import numpy as np
+import pandas as pd
+
+from nlhs_tick_data_hungary.network.network_preparation.sparcc.log_ratio_variance_calculator import LogRatioVarianceCalculator
+
+
 class BasisVarianceCalculator:
-    def __init__(self):
-        pass
+    def __init__(self, data: pd.DataFrame, helper_matrix: np.ndarray):  # lehet pandas a helper_matrix
+        self.data = data
+        self.helper_matrix = helper_matrix
+
+        self.result: (pd.DataFrame | None) = None
+    # TODO: change implementation diagram
+
+    # TODO: log-ratio var importját rövidíteni
 
     def run(self):
-        pass
+        self.calculate_basis_variance()
+
+    def calculate_basis_variance(self):
+        log_ratio_variance_calculator = LogRatioVarianceCalculator(data=self.data)
+        log_ratio_variance_calculator.run()
+
+        log_ratio_variance = log_ratio_variance_calculator.result
+
+        helper_matrix_inv = np.linalg.inv(self.helper_matrix)
+
+        self.result = np.dot(helper_matrix_inv, log_ratio_variance.sum(axis=1))
+
+        self.result[self.result <= 0] = 1e-6
