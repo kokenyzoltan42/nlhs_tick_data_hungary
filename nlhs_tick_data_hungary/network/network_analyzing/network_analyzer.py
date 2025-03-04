76c20a5c97576e6795af739caaa1d3b589ba729b
zoltanocska <zolysta@gmail.com>
2025-03-04 13:53:57 +0100

    Created files and couple of classes

---
+++ nlhs_tick_data_hungary/network/network_analyzing/network_analyzer.py
@@ -0,0 +1,43 @@
+# TODO: calc_ metódusokat ABC-sorrendbe tenni
+class NetworkAnalyzer:
+    def __init__(self):
+        pass
+
+    def calc_network_diameter(self):
+        pass
+
+    def calc_modularity(self):
+        pass
+
+    def calc_average_degree_connectivity(self):
+        pass
+
+    def calc_number_of_communities(self):
+        pass
+
+    def calc_number_of_triangles(self):
+        pass
+
+    def calc_largest_connected_component(self):
+        pass
+
+    def calc_average_path_length(self):
+        pass
+
+    def calc_degree_centrality(self):
+        pass
+
+    def calc_betweenness_centrality(self):
+        pass
+
+    def calc_closeness_centrality(self):
+        pass
+
+    def calc_eigenvector_centrality(self):
+        pass
+
+    def calc_hub_taxa(self):
+        pass
+
+    def run(self):
+        pass
---
+++ nlhs_tick_data_hungary/network/network_preparation/sparcc/log_ratio_variance_calculator.py
@@ -0,0 +1,9 @@
+class LogRatioVarianceCalculator:
+    def __init__(self):
+        pass
+
+    def calc_log_ratio_var(self):
+        pass
+
+    def regenerate_data_with_dirichlet(self):
+        pass
