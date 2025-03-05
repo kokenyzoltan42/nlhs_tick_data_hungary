a28ee0b2a8cc68f4e47f38731acd7473a42e063d
zoltanocska <zolysta@gmail.com>
2025-03-05 02:00:34 +0100

    FInsihed most of the creation of CorrelationCalculator (only missing docstings and comments)

--- nlhs_tick_data_hungary/network/network_preparation/sparcc/correlation_calculator.py
+++ nlhs_tick_data_hungary/network/network_preparation/sparcc/correlation_calculator.py
@@ -1,6 +1,31 @@
+import numpy as np
+import pandas as pd
+
+from nlhs_tick_data_hungary.network.network_preparation.sparcc.basis_variance_calculator import BasisVarianceCalculator
+
+
 class CorrelationCalculator:
-    def __init__(self):
-        pass
+    def __init__(self, data: pd.DataFrame, helper_matrix: np.ndarray):
+        self.data = data
+        self.helper_matrix = helper_matrix
+
+        self.result: (pd.DataFrame | None) = None
+
+        # TODO: change implementation diagram description
 
     def run(self):
-        pass
+        self.substitute_into_formula()
+
+    def substitute_into_formula(self):
+        basis_variance_calculator = BasisVarianceCalculator(data=self.data,
+                                                            helper_matrix=self.helper_matrix)
+        basis_variance_calculator.run()
+
+        basis_variance = basis_variance_calculator.result
+        log_ratio_variance = basis_variance_calculator.log_ratio_variance
+
+        omega_i, omega_j = np.meshgrid(basis_variance, basis_variance)
+
+        numerator = 0.5 * (omega_i + omega_j - log_ratio_variance)
+
+        self.result = numerator / np.sqrt(omega_i) / np.sqrt(omega_j)
