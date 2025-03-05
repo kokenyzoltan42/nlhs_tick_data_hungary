2158b9d7d8af33fa2682d2ae13d97e12bd7d6125
zoltanocska <zolysta@gmail.com>
2025-03-05 10:38:23 +0100

    Added "testing enviroment"

---
+++ nlhs_tick_data_hungary/utils/testing_sparcc.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from nlhs_tick_data_hungary import data_path
+from nlhs_tick_data_hungary.network.network_preparation.sparcc.sparcc_runner import SparCCRunner
+
+
+def main():
+    with open(data_path + f'/fake_data.txt', 'r+') as file:
+        fake_data = pd.read_table(file)
+
+    args = {
+        'n_iter': 50,
+        'x_iter': 10,
+        'tol': 0.1
+    }
+
+    runner = SparCCRunner(df=fake_data,
+                          args=args)
+    result = runner.run()
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
