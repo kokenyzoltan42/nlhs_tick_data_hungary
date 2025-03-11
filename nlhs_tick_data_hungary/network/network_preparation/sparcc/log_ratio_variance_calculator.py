6f0e938819d40f17a8c50b0295f9050c909645d8
zoltanocska <zolysta@gmail.com>
2025-03-11 15:48:47 +0100

    Changed docstrings and variable names

--- nlhs_tick_data_hungary/network/network_preparation/sparcc/log_ratio_variance_calculator.py
+++ nlhs_tick_data_hungary/network/network_preparation/sparcc/log_ratio_variance_calculator.py
@@ -3,19 +3,18 @@ import pandas as pd
 
 
 class LogRatioVarianceCalculator:
-    # TODO: rewrite docstrings
     """
     A class to calculate the log-ratio variance of a set of variables from a DataFrame.
 
-    The log-ratio variance is calculated by comparing the log of the ratio of each pair of OTUs across
-    all samples. This measure captures the variability in the relationships between variables.
+    The log-ratio variance (referenced as T matrix on the Wiki page) is calculated by comparing the log of the ratio
+    of each pair of OTUs across all samples. This measure captures the variability in the relationships between variables.
     """
 
     def __init__(self, data: pd.DataFrame):
         """
         Initialize the LogRatioVarianceCalculator with a given DataFrame.
 
-        :param data: A pandas DataFrame where each row represents a sample and each column represents a variable.
+        :param data: A pandas DataFrame where each row represents a sample and each column represents an OTU.
         """
         self.data = data
 
@@ -23,29 +22,28 @@ class LogRatioVarianceCalculator:
 
     def run(self):
         """
-        Runs the entire process: resampling the data and calculating the log-ratio variance. The log-ratio variance
-        matrix is stored in the `result` attribute.
+        Runs the pipeline of the calculation of the log-ratio variance matrix (T).
         """
         self.calc_log_ratio_var()
 
     def calc_log_ratio_var(self):
         """
-        Calculate the log-ratio variance for each pair of variables across all samples.
+        Calculate the log-ratio variance for each pair of OTUs across all samples.
 
         This method computes the log of the ratio between every pair of variables (i, j) for each
         sample, and then calculates the variance of these log-ratios across all samples.
 
-        The resulting variance matrix represents the variability of the log-ratio between
+        The resulting variance matrix (T) represents the variability of the log-ratio between
         each pair of variables.
         """
         # Convert the DataFrame to a numpy array (of floats)
         variable_data = 1.0 * np.asarray(self.data)
 
-        num_otus, num_samples = variable_data.shape
+        num_samples, num_otus = variable_data.shape
         #num_samples, num_otus = variable_data.shape
 
         # Create a 3D array where each element contains a copy of the variable_data along the 3rd dimension
-        data_expanded = np.tile(variable_data.reshape((num_otus, num_samples, 1)), (1, 1, num_samples))
+        data_expanded = np.tile(variable_data.reshape((num_samples, num_otus, 1)), (1, 1, num_otus))
 
         # Transpose the data along the last two axes
         data_transposed = data_expanded.transpose(0, 2, 1)
