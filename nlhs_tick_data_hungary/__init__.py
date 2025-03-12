623e4af8fe7d391406565e64eefabbe221ba6c7a
zoltanocska <zolysta@gmail.com>
2025-03-12 03:48:35 +0100

    Removed txt files from repository (and with that the data_path). Now it loads from GDrive. testing_sparcc now compares the result with the true correlations via mae and mse

--- nlhs_tick_data_hungary/__init__.py
+++ nlhs_tick_data_hungary/__init__.py
@@ -2,4 +2,3 @@ import os
 
 project_dir = os.path.dirname(os.path.realpath(__file__)).rstrip("nlhs_tick_data_hungary")
 config_path = os.path.join(project_dir, "nlhs_tick_data_hungary", "data", "configs")
-data_path = os.path.join(project_dir, "nlhs_tick_data_hungary", "utils")
---
+++ nlhs_tick_data_hungary/utils/tests/testing_sparcc.py
@@ -0,0 +1,50 @@
+import json
+
+import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+from nlhs_tick_data_hungary import config_path
+from nlhs_tick_data_hungary.data.utils.google_data_downloader import GoogleDataDownloader
+from nlhs_tick_data_hungary.network.network_preparation.sparcc.sparcc_runner import SparCCRunner
+
+
+def main():
+    with open(config_path + f'/links.json', 'r+') as file:
+        links = json.load(file)
+
+    fake_data_dl = GoogleDataDownloader(
+        file_url=links['fake_data_for_sparcc'],
+        file_name='fake_data.txt'
+    )
+
+    true_basis_cor_dl = GoogleDataDownloader(
+        file_url=links['true_basis_cor_for_sparcc'],
+        file_name='true_basis_cor.txt'
+    )
+
+    fake_data = pd.read_table(fake_data_dl.file_path, sep='\t')
+    true_basis_cor = pd.read_table(true_basis_cor_dl.file_path, sep='\t')
+
+    #print(pd.DataFrame(true_basis_cor.iloc[:, 1:]))
+
+    args = {
+        'n_iter': 20,
+        'x_iter': 70,
+        'threshold': 0.1
+    }
+
+    runner = SparCCRunner(df=fake_data.T,
+                          args=args)
+    result = runner.run()
+
+    print(pd.DataFrame(result))
+
+    mae = mean_absolute_error(true_basis_cor.iloc[:, 1:], result)
+    print(f"\n\nMean Absolute Error: {mae}")
+
+    mse = mean_squared_error(true_basis_cor.iloc[:, 1:], result)
+    print(f"\nMean Squared Error: {mse}")
+
+
+if __name__ == '__main__':
+    main()
