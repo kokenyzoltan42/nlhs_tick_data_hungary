cb4983ac1ecd042eb7b1e07739c3dfde24e3dfc7
zoltanocska <zolysta@gmail.com>
2025-03-17 11:05:43 +0100

    Added class for calculating correlations

---
+++ nlhs_tick_data_hungary/network/network_preparation/sparcc/correlation_calculator.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+
+
+class CorrelationCalculator:
+    """
+    This class computes correlation values based on log-ratio variances and estimated basis variances.
+    """
+
+    def __init__(self, log_ratio_variance: np.ndarray, basis_variances: np.ndarray, helper_matrix: np.ndarray):
+        """
+        Initializes the correlation calculator with input data, a helper matrix, and a variation matrix copy.
+
+        :param np.ndarray log_ratio_variance: A matrix containing log-ratio variances for each OTU
+        :param np.ndarray basis_variances: A matrix containing basis variances for each OTU
+        :param np.ndarray helper_matrix: A matrix used for tracking exclusions and modifications.
+        """
+        # Array containing the log-ratio variances (Variation matrix, also referenced as T matrix)
+        self.log_ratio_variance = log_ratio_variance
+        # Array containing the basis variances
+        self.basis_variances = basis_variances
+        # Helper matrix (M)
+        self.helper_matrix = helper_matrix
+        # Attribute to store correlation results
+        self.result: (pd.DataFrame | None) = None
+
+    def run(self):
+        """
+        Executes the correlation calculation process by calling the formula substitution method.
+        """
+        self.substitute_into_formula()
+
+    def substitute_into_formula(self):
+        """
+        Computes correlation values using basis variances and log-ratio variances.
+
+        This method initializes and runs the `BasisVarianceCalculator` to obtain basis variances and the variation
+        matrix. The obtained values are then substituted into the appropriate formula to compute correlations.
+        """
+        # Create a meshgrid for basis variances
+        omega_i, omega_j = np.meshgrid(self.basis_variances, self.basis_variances)
+        # Storing correlations
+        numerator = 0.5 * (omega_i + omega_j - self.log_ratio_variance)
+        # Compute and store the correlation matrix
+        self.result = numerator / np.sqrt(omega_i) / np.sqrt(omega_j)
