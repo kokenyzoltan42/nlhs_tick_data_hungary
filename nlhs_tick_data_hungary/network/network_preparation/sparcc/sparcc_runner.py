27f5305796c9c0f99baf787f804c79fcea6e2236
zoltanocska <zolysta@gmail.com>
2025-03-17 20:14:16 +0100

    Changed arg name and made the algorithm work

--- nlhs_tick_data_hungary/network/network_preparation/sparcc/sparcc_runner.py
+++ nlhs_tick_data_hungary/network/network_preparation/sparcc/sparcc_runner.py
@@ -3,26 +3,24 @@ from typing import Tuple
 import numpy as np
 import pandas as pd
 
-# TODO: ezeke menjenek az __init__-be?
 from nlhs_tick_data_hungary.network.network_preparation.sparcc.basis_variance_calculator import BasisVarianceCalculator
 from nlhs_tick_data_hungary.network.network_preparation.sparcc.correlation_calculator import CorrelationCalculator
-from nlhs_tick_data_hungary.network.network_preparation.sparcc.log_ratio_variance_calculator import LogRatioVarianceCalculator
+from nlhs_tick_data_hungary.network.network_preparation.sparcc.log_ratio_variance_calculator import \
+    LogRatioVarianceCalculator
 
 
 class SparCCRunner:
-    def __init__(self, data: pd.DataFrame, args: dict):
-        self.data = data
+    def __init__(self, df: pd.DataFrame, args: dict):
+        self.df = df
         self.args = args
 
-        self.num_of_components = self.data.shape[1]
+        self.num_of_components = None  # self.df.shape[1]
         self.helper_matrix = None
+        self.data = None
+        self.variances = None
 
-        self.correlations = None
-
-        self.excluded_pairs = None
-        self.excluded_components = None
-
-        self.result = None
+        self.excluded_pairs = []
+        self.excluded_components = np.array([])
 
     # TODO: először úgy implementáld, hogy az excludion is ide kerüljön, utána gondolkozz azon, hogyan lehet külön osztály
 
@@ -44,10 +42,11 @@ class SparCCRunner:
         Each row of the data is treated as the parameters for the Dirichlet distribution,
         which generates(?) a new row of samples based on those parameters.
         """
+        np.random.seed(42)
         self.data = np.apply_along_axis(
             lambda x: np.random.mtrand.dirichlet(x + 1),
             axis=1,
-            arr=self.data
+            arr=self.df
         )
 
     @staticmethod
@@ -92,8 +91,8 @@ class SparCCRunner:
                 raise ValueError("Too many components had to be excluded from the analysis")
             # Update the helper matrix to remove newly excluded components
             for comp in newly_excluded_components:
-                self.variation_matrix_copy[comp, :] = 0
-                self.variation_matrix_copy[:, comp] = 0
+                self.variation_matrix[comp, :] = 0
+                self.variation_matrix[:, comp] = 0
                 self.helper_matrix[comp, :] = 0
                 self.helper_matrix[:, comp] = 0
                 self.helper_matrix[comp, comp] = 1
@@ -102,21 +101,22 @@ class SparCCRunner:
             self.excluded_components = np.array(list(self.excluded_components))
 
     def calc_correlations_with_sparcc(self):
+        log_ratio_variances = LogRatioVarianceCalculator(data=self.data)
+        log_ratio_variances.run()
+        self.variation_matrix = log_ratio_variances.result.copy()
+        self.num_of_components = self.variation_matrix.shape[1]
 
         self.helper_matrix = (np.ones((self.num_of_components, self.num_of_components)) +
                               np.diag([self.num_of_components - 2] * self.num_of_components))
 
-        log_ratio_variances = LogRatioVarianceCalculator(data=self.data)
-        log_ratio_variances.run()
-        variation_matrix = log_ratio_variances.result
-        basis_variances = BasisVarianceCalculator(log_ratio_variance=variation_matrix,
+        basis_variances = BasisVarianceCalculator(log_ratio_variance=self.variation_matrix,
                                                   helper_matrix=self.helper_matrix)
         basis_variances.run()
         correlation_calculator = CorrelationCalculator(basis_variances=basis_variances.result,
-                                                       log_ratio_variance=variation_matrix,
-                                                       helper_matrix=self.helper_matrix)
+                                                       log_ratio_variance=self.variation_matrix  # ide mindegy melyik megy
+                                                       )
         correlation_calculator.run()
-        correlations = correlation_calculator.result
+        correlations = correlation_calculator.result.copy()
 
         for _ in range(self.args['x_iter']):
             to_exclude = self.find_new_excluded_pair(correlations=correlations,
@@ -133,18 +133,17 @@ class SparCCRunner:
             self.helper_matrix[i, i] -= 1
             self.helper_matrix[j, j] -= 1
 
-            # TODO: biztos szükség van transzponálásra?
-            inda, indb = np.transpose(self.excluded_pairs)
-            variation_matrix[inda, indb] = 0
-            variation_matrix.T[inda, indb] = 0
+            inds = tuple(zip(*self.excluded_pairs))
+            self.variation_matrix[inds] = 0
+            self.variation_matrix.T[inds] = 0
 
             self.exclude_components()
 
-            another_basis_variance_calculator = BasisVarianceCalculator(log_ratio_variance=variation_matrix,
+            another_basis_variance_calculator = BasisVarianceCalculator(log_ratio_variance=self.variation_matrix,
                                                                         helper_matrix=self.helper_matrix)
             another_basis_variance_calculator.run()
             basis_variances = another_basis_variance_calculator.result
-            another_correlation_calculator = CorrelationCalculator(log_ratio_variance=log_ratio_variances.result,
+            another_correlation_calculator = CorrelationCalculator(log_ratio_variance=log_ratio_variances.result.copy(),  # Ennek szigorún ennek kell lennie
                                                                    basis_variances=basis_variances)
             another_correlation_calculator.run()
             correlations = another_correlation_calculator.result
@@ -153,4 +152,4 @@ class SparCCRunner:
                 correlations[excluded_component, :] = np.nan
                 correlations[:, excluded_component] = np.nan
 
-        self.result = correlations
+        return correlations
