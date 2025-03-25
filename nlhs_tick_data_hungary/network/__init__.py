58307246f0047d48454efcbf797c76fd604c4ba1
zoltanocska <zolysta@gmail.com>
2025-03-25 17:30:38 +0100

    Changed order of imports to avoid circular import

--- nlhs_tick_data_hungary/network/__init__.py
+++ nlhs_tick_data_hungary/network/__init__.py
@@ -1,8 +1,3 @@
-from nlhs_tick_data_hungary.network.network_preparation.co_occurrence_network_preprocessor import \
-    CoOccurrenceNetworkPreprocessor
-from nlhs_tick_data_hungary.network.network_preparation.general_network_preprocessor import GeneralNetworkPreprocessor
-from nlhs_tick_data_hungary.network.network_creation.network_creator import NetworkCreator
-
 from nlhs_tick_data_hungary.network.sparcc.correlation_updater import CorrelationUpdater
 from nlhs_tick_data_hungary.network.sparcc.basis_variance_calculator import BasisVarianceCalculator
 from nlhs_tick_data_hungary.network.sparcc.correlation_calculator import CorrelationCalculator
@@ -10,3 +5,8 @@ from nlhs_tick_data_hungary.network.sparcc.log_ratio_variance_calculator import 
     LogRatioVarianceCalculator
 from nlhs_tick_data_hungary.network.sparcc.strongly_correlated_pair_excluder import \
     StronglyCorrelatedPairHandler
+
+from nlhs_tick_data_hungary.network.network_preparation.co_occurrence_network_preprocessor import \
+    CoOccurrenceNetworkPreprocessor
+from nlhs_tick_data_hungary.network.network_preparation.general_network_preprocessor import GeneralNetworkPreprocessor
+from nlhs_tick_data_hungary.network.network_creation.network_creator import NetworkCreator
