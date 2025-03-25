6ead2c699ca6ee6c4837ecf469c713b1d60d952d
zoltanocska <zolysta@gmail.com>
2025-03-25 17:29:52 +0100

    Changed order of imports to avoid circular import

--- nlhs_tick_data_hungary/network/__init__.py
+++ nlhs_tick_data_hungary/network/__init__.py
@@ -1,9 +1,9 @@
-from nlhs_tick_data_hungary.network.sparcc.correlation_updater import CorrelationUpdater
 from nlhs_tick_data_hungary.network.network_preparation.co_occurrence_network_preprocessor import \
     CoOccurrenceNetworkPreprocessor
 from nlhs_tick_data_hungary.network.network_preparation.general_network_preprocessor import GeneralNetworkPreprocessor
 from nlhs_tick_data_hungary.network.network_creation.network_creator import NetworkCreator
 
+from nlhs_tick_data_hungary.network.sparcc.correlation_updater import CorrelationUpdater
 from nlhs_tick_data_hungary.network.sparcc.basis_variance_calculator import BasisVarianceCalculator
 from nlhs_tick_data_hungary.network.sparcc.correlation_calculator import CorrelationCalculator
 from nlhs_tick_data_hungary.network.sparcc.log_ratio_variance_calculator import \
