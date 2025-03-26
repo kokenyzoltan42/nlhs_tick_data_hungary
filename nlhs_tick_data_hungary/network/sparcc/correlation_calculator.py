cfce38b0366231277cbf594cb93ba22d2c91488f
kokenyzoltan42 <150195394+kokenyzoltan42@users.noreply.github.com>
2025-03-26 13:51:13 +0100

    Sparcc implementation (#9)
    * changed file name
    
    * Created files for sparcc
    
    * Added class for calculating basis variances
    
    * Added class for calculating correlations
    
    * Added class for calculating log-ratio variances
    
    * Changed the argument name in SparCCRunner
    
    * Added a temporary structure
    
    * Changed arg name back
    
    * Added TEMPORARY (will be moved to GColab) testing environment
    
    * Added TEMPORARY (will be moved to GColab) testing environment
    
    * Removed `helper_matrix` from arguments
    
    * Changed arg name and made the algorithm work
    
    * Changed arg name of SparCCRunner
    
    * Separated the previous class into two: runner and excluder
    
    * Moved the class that handles strongly correlated pairs to a different file
    
    * Added docstrings and comments
    
    * Removed temporary testing environment
    
    * Removed links related to temporary testing of sparcc
    
    * changed the method's name that runs the exclusion pipeline
    
    * Moved imports to the __init__.py in network_preparation directory
    
    * Removed unnecessary comma
    
    * Added line to the end of file
    
    * Created class for calculating the correlations
    
    * MOved some parts of the run method to other mothods
    
    * Updated the method of calculation of correlations
    
    * Changed order of commits
    
    * Updated docstrings comments (variation matrix -> log-ratio variances)
    
    * Moved sparcc sub-module to network module
    
    * Added missing return to the end of method
    
    * Removed parenthesis after initialization of static class and moved args to new line
    
    * Changed order of imports
    
    * Changed order of imports to avoid circular import
    
    * Changed order of imports to avoid circular import
    
    * Changed order of imports to avoid circular import
    
    * Changed order of imports to avoid circular import
    
    * Changed order of imports to avoid circular import
    
    * Changed order of imports to avoid circular import
    
    * Changed order of imports to avoid circular import
    
    * Changed order of imports to avoid circular import
    
    * Changed order of imports and removed some imports to avoid circular import
    
    * Changed import to avoid circular import
    
    * renamed file name to mathc with class name
    
    * Changed import to avoid circular import
    
    * Changed import to avoid circular import
    
    * Changed import to avoid circular import
    
    * Testing imports
    
    * Testing imports
    
    * Testing imports
    
    * Added option to save data and output
    
    * Changed order of imports
    
    * Added annotation
    
    * Converted sparcc results to pd.dataframe
    
    * removed transposing
    
    * renamed saving method

---
+++ nlhs_tick_data_hungary/network/sparcc/correlation_calculator.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+
+
+class CorrelationCalculator:
+    """
+    This class computes correlation values based on log-ratio variances and estimated basis variances.
+    """
+
+    def __init__(self, log_ratio_variance: np.ndarray, basis_variances: np.ndarray):
+        """
+        Initializes the correlation calculator with input data, a helper matrix, and the log_ratio_variances.
+
+        :param np.ndarray log_ratio_variance: A matrix containing log-ratio variances for each OTU
+        :param np.ndarray basis_variances: A matrix containing basis variances for each OTU
+        """
+        # Array containing the log-ratio variances (often referenced as variation matrix, or T matrix)
+        self.log_ratio_variance = log_ratio_variance
+        # Array containing the basis variances
+        self.basis_variances = basis_variances
+        # Attribute to store correlation results
+        self.result: (pd.DataFrame | None) = None
+
+    def run(self):
+        """
+        Executes the correlation calculation process by calling the formula substitution method.
+        """
+        self.substitute_into_formula()
+
+    def substitute_into_formula(self):
+        """
+        Computes correlation values using basis variances and log-ratio variances.
+
+        This method initializes and runs the `BasisVarianceCalculator` to obtain basis variances and
+        the log-ratio variances. The obtained values are then substituted into the appropriate formula
+        to compute correlations.
+        """
+        # Create a meshgrid for basis variances
+        omega_i, omega_j = np.meshgrid(self.basis_variances, self.basis_variances)
+        # Storing correlations
+        numerator = 0.5 * (omega_i + omega_j - self.log_ratio_variance)
+        # Compute and store the correlation matrix
+        self.result = numerator / np.sqrt(omega_i) / np.sqrt(omega_j)
---
+++ nlhs_tick_data_hungary/network/sparcc/correlation_updater.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from nlhs_tick_data_hungary.network.sparcc import BasisVarianceCalculator
+from nlhs_tick_data_hungary.network.sparcc import CorrelationCalculator
+
+
+class CorrelationUpdater:
+    """
+    Manages the computation and updating of correlation matrices.
+    This class handles the execution of BasisVarianceCalculator and CorrelationCalculator
+    to ensure that correlation matrices are properly recalculated after exclusions.
+    """
+
+    @staticmethod
+    def calculate_correlation(log_ratio_variances: np.ndarray, helper_matrix: np.ndarray) -> np.ndarray:
+        """
+        Computes the updated correlation matrix using the provided log-ratio variances and helper matrix.
+
+        :param np.ndarray log_ratio_variances: An array containing the log-ratio variances
+        :param np.ndarray helper_matrix: An array containing the helper matrix (also referenced as the $M$ matrix)
+        :return np.ndarray: The updated correlation matrix.
+        """
+        variance_calculator = BasisVarianceCalculator(log_ratio_variance=log_ratio_variances,
+                                                      helper_matrix=helper_matrix)
+        variance_calculator.run()
+        basis_variances = variance_calculator.result
+
+        correlation_calculator = CorrelationCalculator(log_ratio_variance=log_ratio_variances,
+                                                       basis_variances=basis_variances)
+        correlation_calculator.run()
+        return correlation_calculator.result
