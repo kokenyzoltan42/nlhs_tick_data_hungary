936ca14cfd84ef55b1f63691dfdbfb9c7e14dc59
zoltanocska <zolysta@gmail.com>
2025-04-09 00:18:21 +0200

    Updated docstrings and changed order of methods

--- nlhs_tick_data_hungary/network/sparcc/clr_calculator.py
+++ nlhs_tick_data_hungary/network/sparcc/clr_calculator.py
@@ -10,26 +10,36 @@ class CLRCalculator:
         """
         Initializes the CLRCalculator with the given data.
 
-        Parameters:
-        data (np.ndarray): A NumPy array containing the input data.
+        :param np.ndarray data (np.ndarray): A NumPy array containing the input data.
         """
         self.data = data.T
 
     def run(self) -> np.ndarray:
         """
-        Executes the CLR transformation and computes correlations.
+        Executes the CLR transformation and computes correlations
 
-        Returns:
-        np.ndarray: Correlation matrix of CLR-transformed data.
+        :return np.ndarray: Correlation matrix of CLR-transformed data.
         """
         return self.run_clr()
+
+    def run_clr(self) -> np.ndarray:
+        """
+        Computes the CLR transformation and calculates the correlation matrix.
+
+        :return np.ndarray: Correlation matrix of CLR-transformed data.
+        """
+        z = self.clr()
+
+        # Compute correlation coefficients from the CLR-transformed data
+        correlations = np.corrcoef(z)
+
+        return correlations
 
     def clr(self) -> np.ndarray:
         """
         Performs Centered Log-Ratio (CLR) transformation on the dataset.
 
-        Returns:
-        np.ndarray: CLR-transformed dataset.
+        :return np.ndarray: CLR-transformed dataset.
         """
 
         # Apply log transformation after adding 1 to avoid log(0)
@@ -39,17 +49,3 @@ class CLRCalculator:
         mean = np.mean(log_data, axis=0, keepdims=True)
 
         return log_data - mean
-
-    def run_clr(self) -> np.ndarray:
-        """
-        Computes the CLR transformation and calculates the correlation matrix.
-
-        Returns:
-        np.ndarray: Correlation matrix of CLR-transformed data.
-        """
-        z = self.clr()
-
-        # Compute correlation coefficients from the CLR-transformed data
-        correlations = np.corrcoef(z)
-
-        return correlations
