e00d1ff7cff6c4f6af15d2f0117a05d4a26d5888
zoltanocska <zolysta@gmail.com>
2025-04-08 14:03:29 +0200

    Testing

--- nlhs_tick_data_hungary/network/sparcc/sparcc_runner.py
+++ nlhs_tick_data_hungary/network/sparcc/sparcc_runner.py
@@ -94,7 +94,7 @@ class SparCCRunner:
         """
         self.data = np.apply_along_axis(
             lambda x: np.random.mtrand.dirichlet(x + 1),
-            axis=1,
+            axis=0,
             arr=self.df
         )
 
