2cb27218f7ec910835e781574f7ac5d90f57e110
zoltanocska <zolysta@gmail.com>
2025-04-08 08:16:29 +0200

    Finalized structure of CLRCalculator, might need some changes (axis)

--- nlhs_tick_data_hungary/network/sparcc/clr_calculator.py
+++ nlhs_tick_data_hungary/network/sparcc/clr_calculator.py
@@ -1,3 +1,26 @@
+import numpy as np
+
+
 class CLRCalculator:
-    def __init__(self):
+    def __init__(self, data: np.ndarray):
+        self.data = data
+
+    def run(self):
         pass
+
+    def clr(self) -> np.ndarray:
+        frame_temp = np.log(self.data)
+
+        mean = np.mean(frame_temp,
+                       axis=1,  # vagy 0?
+                       keepdims=True)
+        clr = frame_temp - mean
+
+        return clr
+
+    def run_clr(self):
+        z = self.clr()
+        correlations = np.corrcoef(z,
+                                   rowvar=False)  # vagy True?
+
+        return correlations
