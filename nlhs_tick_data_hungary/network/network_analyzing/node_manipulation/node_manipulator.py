b8b296622e9ef75aade459460add3b891ec954a5
zoltanocska <zolysta@gmail.com>
2025-04-09 04:45:45 +0200

    Updated docstrings, type annotations for methods

--- nlhs_tick_data_hungary/network/network_analyzing/node_manipulation/node_manipulator.py
+++ nlhs_tick_data_hungary/network/network_analyzing/node_manipulation/node_manipulator.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import pandas as pd
 
 from nlhs_tick_data_hungary.network.network_analyzing.node_manipulation.node_attacker import NodeAttacker
 from nlhs_tick_data_hungary.network.network_analyzing.node_manipulation.node_defender import NodeDefender
@@ -6,10 +7,9 @@ from nlhs_tick_data_hungary.network.network_analyzing.node_manipulation.node_def
 
 class NodeManipulator:
     """
-    A class to manipulate nodes in a network graph to analyze robustness and vulnerability.
+    A class to manipulate nodes in a network to analyze robustness and vulnerability.
     Supports two simulation modes: defending (adding nodes) and attacking (removing nodes).
     """
-    # TODO: Update docstrings if necessary (probably)
     def __init__(self, network: nx.Graph, config: dict):
         """
         Initializes the NodeManipulator with a network and configuration settings.
@@ -19,7 +19,7 @@ class NodeManipulator:
         - 'num_of_connections': The number of connections to establish at each node addition (only used
         during 'defending' simulations)
         - `nodes_to_add`: Number of nodes to add (only used during 'defending' simulations)
-        - 'metric': What metric to use, e.g.: 'APL', 'LCC' (only used during 'defending' simulations)
+        - 'defending_metric': What metric to use, e.g.: 'APL', 'LCC' (only used during 'defending' simulations)
         - 'attack_type': Type node removal strategy, e.g.: initial_betweenness, cascading_betweenness,
          random (only used during 'attacking' simulations)
 
@@ -32,11 +32,11 @@ class NodeManipulator:
         self.network = network
         self.config = config
 
-    def run_simulation(self) -> dict:
+    def run_simulation(self) -> pd.DataFrame:
         """
         Runs the simulation based on the selected mode.
 
-        :return dict: A dictionary with simulation results.
+        :return pd.DataFrame: A dataframe with simulation results.
         """
         simulation_modes = {
             'defending': NodeDefender(network=self.network, config=self.config),
