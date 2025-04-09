3b92036b3283b2835734f708e1655580714f9892
zoltanocska <zolysta@gmail.com>
2025-04-09 02:15:05 +0200

    Defined values in constructor

--- nlhs_tick_data_hungary/network/network_analyzing/node_manipulation/node_adder.py
+++ nlhs_tick_data_hungary/network/network_analyzing/node_manipulation/node_adder.py
@@ -4,8 +4,12 @@ from nlhs_tick_data_hungary.network.network_analyzing.node_manipulation.metric_c
 
 
 class NodeAdder:
-    def __init__(self):
-        pass
+    def __init__(self, network: nx.Graph, config: dict):
+        self.network = network
+        self.config = config
+
+        self.nodes_added = []  # Itt lehet elég egy for ciklus
+        self.metric_results = []
 
     def run(self):
         pass
